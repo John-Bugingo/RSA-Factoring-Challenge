623eb62ed24b206d3d26894c69ba6f048963a1e6
John-Bugingo <johnbugingo12@gmail.com>
2022-06-26 16:22:32 -0700

    RSA Factoring Challenge

--- factors.py
+++ factors.py
@@ -1,5 +1,8 @@
-def openfile(filename):
-    with open(filename "r") as fd:
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    with open(sys.argv[1], "r") as fd:
         readlins = fd.readlines()
     for line in readlins:
         num = int(line)
